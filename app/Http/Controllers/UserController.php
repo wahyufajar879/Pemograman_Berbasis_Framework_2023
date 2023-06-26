d440ce97bcef1a26c9a5579267969ebec7290374
wahyufajar879 <goesfa3110@gmail.com>
2023-06-26 10:55:18 +0700

    laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function register(request $req){
+        $user = new User;
+        $user->name= $req->input("name");
+        $user->email= $req->input("email");
+        $user->password= Hash::make($req->input("password"));
+        $user->save();
+
+        return $user;
+
+    }
+
+    function login(request $req){
+        $user = User::where('email', $req->email)->first();
+        if(!$user || !Hash::check($req->password, $user->password)){
+            return ["Error"=>"Sorry, Email or Password doest't match"];
+        }
+        return  $user;
+    }
+}
